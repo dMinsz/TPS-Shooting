82f7eb0bd51f4723d08fcf6435ee95f856453322
dMinsz <dsminzks@gmail.com>
2023-05-31 19:35:42 +0900

    feat: Add Animation Layer for Reload, Fire Animation
    상반신 만 장전, 발사 애니메이션 가능하게 제작완료

---
+++ Assets/Scripts/PlyerShooter.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlyerShooter : MonoBehaviour
+{
+    //[SerializeField] private Rig aimRig;
+    [SerializeField] private float reloadTime;
+    //[SerializeField] private WeaponHolder weaponHolder;
+
+    private Animator animator;
+    private bool reloading;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Fire()
+    {
+        //weaponHolder.Fire();
+        animator.SetTrigger("Fire");
+    }
+
+    private void OnFire(InputValue value)
+    {
+        if (reloading)
+            return;
+
+        Fire();
+    }
+
+    private void OnReload(InputValue value)
+    {
+        StartCoroutine(ReloadRoutine());
+    }
+
+    IEnumerator ReloadRoutine()
+    {
+        animator.SetTrigger("Reloading");
+        //aimRig.weight = 0f;
+        reloading = true;
+        yield return new WaitForSeconds(reloadTime);
+        //aimRig.weight = 0.6f;
+        reloading = false;
+    }
+}
